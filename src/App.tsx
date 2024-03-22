099b5f55d0e26606ac1f31c22bcec43bf7a9b66c
JsusMarcano <jesusjaviermarcanomora.2005@gmail.com>
2024-03-22 07:42:44 -0400

    feat: create star screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
+import { StartScreen } from './views/startScreen'
 
 function App() {
   return (
-    <main className="min-h-screen grid place-items-center bg-stone-100">
-      <button className="py-10 px-16 border-2 border-stone-900 bg-lime-200 rounded-xl brutal-shadow">
-        <h1 className="font-hero text-5xl text-center text-stone-900">Start Trivia</h1>
-      </button>
+    <main className="min-h-screen grid place-items-center bg-stone-100 px-6">
+      <StartScreen />
     </main>
   )
 }
---
+++ src/views/startScreen.tsx
@@ -0,0 +1,7 @@
+export function StartScreen() {
+  return (
+    <button className="py-10 px-16 border-2 border-stone-900 bg-lime-200 rounded-xl brutal-shadow-lg">
+      <h1 className="font-hero text-5xl text-center text-stone-950">Start Trivia</h1>
+    </button>
+  )
+}
