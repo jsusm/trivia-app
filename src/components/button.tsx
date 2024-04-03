b06bd08351339034af20d38536e7976e2904ba96
JsusMarcano <jesusjaviermarcanomora.2005@gmail.com>
2024-04-03 11:41:27 -0400

    feat: change button shadow color, for more consistency

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,7 +7,7 @@ export const Button = forwardRef<HTMLButtonElement, ComponentProps<"button">>(
     <button
       ref={ref}
       className={cx(
-        "brutal-shadow flex items-center rounded-xl border-2 border-stone-900 bg-lime-200 px-8 py-2 font-mono disabled:cursor-not-allowed disabled:bg-lime-50 disabled:text-stone-400",
+        "brutal-shadow flex items-center rounded-xl border-2 border-stone-900 bg-lime-200 px-8 py-2 font-mono disabled:cursor-not-allowed disabled:bg-stone-400 disabled:text-stone-400",
         props.className,
       )}
       {...props}
