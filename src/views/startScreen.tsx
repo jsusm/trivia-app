b1bbe66a71330de8f4f74dd3913efe58db1d00cd
JsusMarcano <jesusjaviermarcanomora.2005@gmail.com>
2024-04-03 11:40:42 -0400

    feat: add description in start screen

--- src/views/startScreen.tsx
+++ src/views/startScreen.tsx
@@ -4,13 +4,25 @@ export interface StartScreenProps {
 
 export function StartScreen({ onStart }: StartScreenProps) {
   return (
-    <button
-      onClick={onStart}
-      className="brutal-shadow-lg rounded-xl border-2 border-stone-900 bg-lime-200 px-16 py-10"
-    >
-      <h1 className="text-center font-hero text-5xl text-stone-950">
+    <div className="flex flex-col items-center gap-8">
+      <p className="max-w-screen-sm text-center font-hero text-3xl text-stone-950">
+        This trivia consists of 10 questions about computer science and
+        computers in general
+      </p>
+      <div>
+        <p className="max-w-screen-sm text-center font-hero text-3xl text-stone-950">
+          Do you think you can get all 10 right?
+        </p>
+        <p className="text-center font-mono text-sm text-red-800">
+          (it's hard, at least for me)
+        </p>
+      </div>
+      <button
+        onClick={onStart}
+        className="brutal-shadow-lg rounded-xl border-2 border-stone-800 bg-lime-200 px-8 py-6 font-hero text-3xl text-stone-800 animate-in fade-in"
+      >
         Start Trivia
-      </h1>
-    </button>
+      </button>
+    </div>
   );
 }
